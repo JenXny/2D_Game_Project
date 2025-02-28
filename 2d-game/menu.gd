544f74a6f323f25341e160b24c55f347b6bf892b
ameehan122 <ameehan12@fordham.edu>
2025-02-28 12:37:12 -0500

    started game files
    made the main menu and started off the main game file

---
+++ 2d-game/menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+
+func _on_load_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
