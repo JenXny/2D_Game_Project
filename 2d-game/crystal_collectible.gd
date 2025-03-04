34ce3cdb45a842460edfddb4e4d02bebae19a10e
ameehan122 <ameehan12@fordham.edu>
2025-03-04 13:06:34 -0500

    added random placement of crystals and rocks

---
+++ 2d-game/crystal_collectible.gd
@@ -0,0 +1,22 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+var rockwalltexture1x1 = preload("res://rock_walls_texture_1x_1.tscn")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	var instance = rockwalltexture1x1.instantiate()
+	$"..".add_child(instance)
+	instance.position.x = randi_range(58,400)
+	instance.position.y = randi_range(58,400)
+	position.x = randi_range(58,400)
+	position.y = randi_range(58,400)
+
+
+#func _process():
+	
--- 2d-game/game.gd
+++ 2d-game/game.gd
@@ -8,7 +8,14 @@ var crystalcollectible = preload("res://crystal_collectible.tscn")
 var rockwalltexture1x1 = preload("res://rock_walls_texture_1x_1.tscn")
 
 
+#func _process():
+	
+
 func _ready():
 	var instance = crystalcollectible.instantiate()
 	add_child(instance)
+	instance.position.x = randi_range(58,1094)
+	instance.position.y = randi_range(58,1094)
+	
+	
 	pass # Replace with function body.
