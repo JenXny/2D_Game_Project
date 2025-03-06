53126ac3f281ff8157049701ebb75560188f7562
mhoque16 <mhoque16@fordham.edu>
2025-03-06 14:16:19 -0500

    Update menu.gd

--- 2d-game/menu.gd
+++ 2d-game/menu.gd
@@ -1,5 +1,5 @@
 extends Control
-
+# functioning menu
 # Called when the node enters the scene tree for the first time
 func _ready() -> void:
 	# Connecting button signals (Ensure buttons are named correctly in the scene)
