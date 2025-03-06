c7186c6fb0cb9eef61db3f4b2032bd301a106734
ameehan122 <ameehan12@fordham.edu>
2025-03-06 15:44:42 -0500

    Added the Game Over, wall detection, and rock collision detection. Also implemented further movement indicators

--- 2d-game/game.gd
+++ 2d-game/game.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var score : int
 var game_started : bool = false
-
+var player_data : Array
 
 var crystalcollectible = preload("res://crystal_collectible.tscn")
 var rockwalltexture1x1 = preload("res://rock_walls_texture_1x_1.tscn")
@@ -17,5 +17,16 @@ func _ready():
 	instance.position.x = randi_range(58,1094)
 	instance.position.y = randi_range(58,1094)
 	
+		
+func end_game():
+	game_started = false
+	print ("Game Over")
+
+func _on_game_over_menu_restart():
+	#new_game()
 	
 	pass # Replace with function body.
+
+
+func _on_border_body_entered(body: Node2D) -> void:
+	end_game()
