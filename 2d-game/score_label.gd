de79bd971607586140c174a66631df86beda9334
JenXny <jvivarvaldiviezo@fordham.edu>
2025-03-11 12:24:06 -0400

    Centered Text Score Box

--- 2d-game/score_label.gd
+++ 2d-game/score_label.gd
@@ -6,8 +6,7 @@ var score = 0
 	
 func increaseScore(amount): 
 	score += amount
-	$ScoreLabel.text = str(score)
-
+	$ScorePanel/HBoxContainer/ScoreLabel.text = str(score)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
